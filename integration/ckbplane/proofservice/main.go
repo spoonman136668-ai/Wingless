02c8b6103893f2ed3590cad01640fc4f1fc48e25
spoonman136668-ai <spoonman136668@gmail.com>
2026-09-12 04:05:22 -0400

    wingless: make proof-service cancellation observable

--- integration/ckbplane/proofservice/main.go
+++ integration/ckbplane/proofservice/main.go
@@ -4,15 +4,22 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	ckbplane "github.com/spoonman136668-ai/Wingless/integration/ckbplane"
 )
 
-type proofEngine struct{}
+type proofEngine struct {
+	marker string
+}
 
-func (proofEngine) Run(ctx context.Context, req ckbplane.TransportRequest) (ckbplane.CandidateEnvelope, error) {
+func (e proofEngine) Run(ctx context.Context, req ckbplane.TransportRequest) (ckbplane.CandidateEnvelope, error) {
 	if strings.Contains(req.Title, "CANCEL_PROOF") {
+		if err := os.WriteFile(e.marker, []byte("active\n"), 0600); err != nil {
+			return ckbplane.CandidateEnvelope{}, err
+		}
+		defer os.Remove(e.marker)
 		<-ctx.Done()
 		return ckbplane.CandidateEnvelope{}, ctx.Err()
 	}
@@ -58,7 +65,14 @@ func main() {
 		fmt.Fprintln(os.Stderr, "WINGLESS_PROOF_SERVICE_SESSION_REQUIRED")
 		os.Exit(2)
 	}
-	if err := ckbplane.ServeLoopbackService(context.Background(), session, proofEngine{}, os.Stdout); err != nil {
+	exe, err := os.Executable()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	marker := filepath.Join(filepath.Dir(exe), "wingless-proof-active.marker")
+	_ = os.Remove(marker)
+	if err := ckbplane.ServeLoopbackService(context.Background(), session, proofEngine{marker: marker}, os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
