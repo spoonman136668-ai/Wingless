96ceed54b84381cf37697ea37571d33ee4cc3239
spoonman136668-ai <spoonman136668@gmail.com>
2026-09-24 16:38:55 -0400

    research: add UP-59B probe

---
+++ cmd/unitary-up59b-confidence-long-horizon/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/spoonman136668-ai/Wingless/unitary"
+)
+
+func main() {
+	r, err := unitary.RunUP59B()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(r); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
