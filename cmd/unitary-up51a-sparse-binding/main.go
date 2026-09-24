2b35205f2aafde6c5ec162fb0faeb527527cf7a5
spoonman136668-ai <spoonman136668@gmail.com>
2026-09-24 06:39:17 -0400

    research: add UP-51A sparse binding probe

---
+++ cmd/unitary-up51a-sparse-binding/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/spoonman136668-ai/Wingless/unitary"
+)
+
+func main() {
+	result, err := unitary.RunUP51A()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(result); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
