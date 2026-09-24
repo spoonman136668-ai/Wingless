97b4175de655ede774fd013f063439b9e933a9f6
spoonman136668-ai <spoonman136668@gmail.com>
2026-09-24 12:57:28 -0400

    research: add UP-53A identifiability probe

---
+++ cmd/unitary-up53a-identifiability/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+ "encoding/json"
+ "fmt"
+ "os"
+ "github.com/spoonman136668-ai/Wingless/unitary"
+)
+
+func main(){
+ r,e:=unitary.RunUP53A()
+ if e!=nil{fmt.Fprintln(os.Stderr,e);os.Exit(1)}
+ enc:=json.NewEncoder(os.Stdout);enc.SetIndent("","  ")
+ if e:=enc.Encode(r);e!=nil{fmt.Fprintln(os.Stderr,e);os.Exit(1)}
+}
