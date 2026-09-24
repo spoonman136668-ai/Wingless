559dc39d775c91cf37603d9a282580f697410c73
spoonman136668-ai <spoonman136668@gmail.com>
2026-09-24 06:17:03 -0400

    research: add UP-48C stress probe

---
+++ cmd/unitary-up48c-scale-stress/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/spoonman136668-ai/Wingless/unitary"
+)
+
+func main() {
+	result, err := unitary.RunUP48C()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(result); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
