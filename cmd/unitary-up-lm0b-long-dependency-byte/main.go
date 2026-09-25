f4ee55f04da9f1052ae89ae7c19bac66ed99d47e
spoonman136668-ai <spoonman136668@gmail.com>
2026-09-25 16:32:07 -0400

    research: add UP-LM0B probe command

---
+++ cmd/unitary-up-lm0b-long-dependency-byte/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	"encoding/json"
+	"fmt"
+	"os"
+	"github.com/spoonman136668-ai/Wingless/unitary"
+)
+
+func main(){
+	r,err:=unitary.RunUPLM0B()
+	if err!=nil{fmt.Fprintln(os.Stderr,err);os.Exit(1)}
+	enc:=json.NewEncoder(os.Stdout);enc.SetIndent("","  ")
+	if err:=enc.Encode(r);err!=nil{fmt.Fprintln(os.Stderr,err);os.Exit(1)}
+}
