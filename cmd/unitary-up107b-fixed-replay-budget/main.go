8adf5ccb1ce485af21bc6b0d819b63b5075a62d8
spoonman136668-ai <spoonman136668@gmail.com>
2026-09-26 04:42:07 -0400

    research: add UP-107B probe command

---
+++ cmd/unitary-up107b-fixed-replay-budget/main.go
@@ -0,0 +1,3 @@
+package main
+import("encoding/json";"fmt";"os";"github.com/spoonman136668-ai/Wingless/unitary")
+func main(){r,e:=unitary.RunUP107B();if e!=nil{fmt.Fprintln(os.Stderr,e);os.Exit(1)};enc:=json.NewEncoder(os.Stdout);enc.SetIndent("","  ");if e:=enc.Encode(r);e!=nil{fmt.Fprintln(os.Stderr,e);os.Exit(1)}}
